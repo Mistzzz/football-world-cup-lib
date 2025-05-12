97c61ad86ac4dfcc657a052c275e3f2c8e448254
Michal Martowicz <michal.martowicz@crowtech.pl>
2025-05-12 19:03:14 +0200

    Add test to updateMatch

--- src/test/java/pl/football/worldcup/storage/InMemoryStorageTest.java
+++ src/test/java/pl/football/worldcup/storage/InMemoryStorageTest.java
@@ -1,5 +1,6 @@
 package pl.football.worldcup.storage;
 
+import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -59,5 +60,48 @@ class InMemoryStorageTest {
 
         // THEN
         assertEquals("Given match already exist in storage", exception.getMessage());
+    }
+
+    @Test
+    void shouldUpdateMatchSuccessfully() {
+        // GIVEN
+        FootballMatch match = FootballMatch.builder()
+                .id(0L)
+                .homeTeam(HOME_TEAM)
+                .awayTeam(AWAY_TEAM)
+                .build();
+        FootballMatch footballMatch = matchStorage.saveMatch(match);
+        LocalDateTime startTime = LocalDateTime.now();
+        footballMatch = FootballMatch.builder()
+                .id(footballMatch.id())
+                .homeTeam(footballMatch.homeTeam())
+                .awayTeam(footballMatch.awayTeam())
+                .startTime(startTime)
+                .build();
+
+        // WHEN
+        FootballMatch footballMatchUpdated = matchStorage.updateMatch(footballMatch);
+
+        assertEquals(1, storage.size());
+        FootballMatch storedMatch = storage.get(footballMatch.id());
+        assertEquals(storedMatch.homeTeam(), footballMatch.homeTeam());
+        assertEquals(storedMatch.awayTeam(), footballMatch.awayTeam());
+        assertEquals(storedMatch.startTime(), footballMatch.startTime());
+    }
+
+    @Test
+    void shouldUpdateMatchWithExceptionMatchNotExists() {
+        // GIVEN
+        FootballMatch match = FootballMatch.builder()
+                .id(1L)
+                .homeTeam(HOME_TEAM)
+                .awayTeam(AWAY_TEAM)
+                .build();
+
+        // WHEN
+        MatchStorageException exception = assertThrows(MatchStorageException.class, () -> matchStorage.updateMatch(match));
+
+        // THEN
+        assertEquals("Given match not exist in storage", exception.getMessage());
     }
 }
